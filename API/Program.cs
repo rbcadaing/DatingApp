f2449bb70374102dae94982880a5d84210262b7a
Rommel Cadaing <rbcadaing@gmail.com>
2020-10-29 16:10:43 +0800

    End of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +14,26 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // Seed Auup user data implementation
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+
+                // Seed Auup user datacd 
+                await Seed.SeedUser(context);
+            }
+            catch (Exception ex)
+            {
+                var Logger = services.GetRequiredService<ILogger<Program>>();
+                Logger.LogError(ex, "An Error occured during migration");
+            }
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
